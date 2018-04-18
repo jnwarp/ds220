2ecb621fc047b21b718c1326c6bc3481885d37e2
James Warp <james@jnwarp.com>
2018-04-17 21:43:14 -0400

    Add function for S&P500

--- process.py
+++ process.py
@@ -4,20 +4,20 @@ import numpy as np
 class bitpayUSD():
     def __init__(self):
         self.preprocess()
-        print(self.df)
+        print(self.df.describe())
 
     def preprocess(self, period='D'):
-        # import the USD price data
+        # import the bitcoin USD price data
         self.df = pd.read_csv(
             'raw/bitpayUSD.csv',
             header=None,
-            names=['time', 'usd', 'vol'])
+            names=['date', 'usd', 'vol'])
 
         # convert unix timestamp into usable number
-        self.df['time'] = pd.to_datetime(self.df['time'], unit='s')
+        self.df['date'] = pd.to_datetime(self.df['date'], unit='s')
 
         # make timestamp the index
-        self.df.set_index('time', inplace=True)
+        self.df.set_index('date', inplace=True)
 
         # get daily totals instead of hourly info
         self.df = self.df.resample(period, how={'usd': 'ohlc', 'vol': np.sum})
@@ -25,5 +25,19 @@ class bitpayUSD():
         # drop empty rows
         self.df = self.df.dropna(axis=0)
 
+class GSPC():
+    def __init__(self):
+        self.preprocess()
+        print(self.df.describe())
+
+    def preprocess(self):
+        # import S&P500 price data
+        self.df = pd.read_csv('raw/GSPC.csv')
+
+        self.df.columns = ['date', 'open', 'high', 'low', 'close', 'adj_close', 'vol']
+
+
 bp = bitpayUSD()
-bp.df.to_csv('processed/bitpayUSD.csv')
+#bp.df.to_csv('processed/bitpayUSD.csv')
+
+gspc = GSPC()
