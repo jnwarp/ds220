57c01e4b46cbc934f05cbc33b5e6622f951fde9a
James Warp <james@jnwarp.com>
2018-04-24 22:28:56 -0400

    Add granger causation

--- GrangerCausality/granger causation.R
+++ GrangerCausality/granger causation.R
@@ -1,6 +1,25 @@
+# check data
 bitstock <- read.csv(file.choose())
 head(bitstock)
 attach(bitstock)
-par(mfrow=c(2,1))
-plot.ts(bitcoin)
-plot.ts(stock)
+par(mar=c(1,1,1,1))
+plot.ts(close_btc)
+plot.ts(close_sp500)
+
+# make stationary
+library(forecast)
+library(lmtest)
+ndiffs(close_btc)
+ndiffs(close_sp500)
+
+# differenced time series
+btc <- diff(close_btc)
+sp500 <- diff(close_sp500)
+plot.ts(btc)
+plot.ts(sp500)
+
+# does sp500 granger cause btc?
+grangertest(sp500 ~ btc, order=4)
+
+# does btc granger cause sp500?
+grangertest(btc ~ sp500, order=4)
